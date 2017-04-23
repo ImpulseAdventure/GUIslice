865154f1570f919cde99dcedbac6d4014ff930d6
Calvin Hass <web@impulseadventure.com>
2017-04-23 05:05:42 -0700

    Add examples gslc-ex09, gslc-ex10

---
+++ tests/gslc-ex09.c
@@ -0,0 +1,213 @@
+//
+// GUIslice Library Examples
+// - Calvin Hass
+// - http://www.impulseadventure.com/elec/guislice-gui.html
+// - Example 09 (LINUX):
+//     Demonstrate radial and ramp controls
+//     NOTE: The ramp control is still beta and subject to change
+//
+#include "GUIslice.h"
+#include "GUIslice_ex.h"
+#include "GUIslice_drv.h"
+
+#include "unistd.h"   // For usleep() //xxx
+
+// Defines for resources
+#define FONT_DROID_SANS "/usr/share/fonts/truetype/droid/DroidSans.ttf"
+
+// Enumerations for pages, elements, fonts, images
+enum {E_PG_MAIN};
+enum {E_ELEM_BOX,E_ELEM_BTN_QUIT,E_ELEM_COLOR,
+      E_RADIAL,E_RAMP,E_SLIDER,E_ELEM_TXT_COUNT};
+enum {E_FONT_BTN,E_FONT_TXT,E_FONT_HEAD,E_FONT_TITLE};
+
+bool      m_bQuit = false;
+
+// Free-running counter for display
+unsigned  m_nCount = 0;
+
+// Instantiate the GUI
+#define MAX_PAGE            1
+#define MAX_FONT            5
+#define MAX_ELEM_PG_MAIN    17
+
+gslc_tsGui                  m_gui;
+gslc_tsDriver               m_drv;
+gslc_tsFont                 m_asFont[MAX_FONT];
+gslc_tsPage                 m_asPage[MAX_PAGE];
+gslc_tsElem                 m_asPageElem[MAX_ELEM_PG_MAIN];
+gslc_tsElemRef              m_asPageElemRef[MAX_ELEM_PG_MAIN];
+
+gslc_tsXGauge               m_sXGauge,m_sXGauge1;
+gslc_tsXSlider              m_sXSlider;
+
+
+// Configure environment variables suitable for display
+// - These may need modification to match your system
+//   environment and display type
+// - Defaults for GSLC_DEV_FB and GSLC_DEV_TOUCH are in GUIslice_config.h
+// - Note that the environment variable settings can
+//   also be set directly within the shell via export
+//   (or init script).
+//   - eg. export TSLIB_FBDEVICE=/dev/fb1
+void UserInitEnv()
+{
+#if defined(DRV_DISP_SDL1) || defined(DRV_DISP_SDL2)
+  setenv((char*)"FRAMEBUFFER",GSLC_DEV_FB,1);
+  setenv((char*)"SDL_FBDEV",GSLC_DEV_FB,1);
+  setenv((char*)"SDL_VIDEODRIVER",GSLC_DEV_VID_DRV,1);
+#endif  
+
+#if defined(DRV_TOUCH_TSLIB)
+  setenv((char*)"TSLIB_FBDEVICE",GSLC_DEV_FB,1);
+  setenv((char*)"TSLIB_TSDEVICE",GSLC_DEV_TOUCH,1); 
+  setenv((char*)"TSLIB_CALIBFILE",(char*)"/etc/pointercal",1);
+  setenv((char*)"TSLIB_CONFFILE",(char*)"/etc/ts.conf",1);
+  setenv((char*)"TSLIB_PLUGINDIR",(char*)"/usr/local/lib/ts",1);  
+#endif
+   
+}
+
+// Define debug message function
+static int16_t DebugOut(char ch) { fputc(ch,stderr); return 0; }
+
+// Quit button callback
+bool CbBtnQuit(void* pvGui,void *pvElem,gslc_teTouch eTouch,int16_t nX,int16_t nY)
+{
+  if (eTouch == GSLC_TOUCH_UP_IN) {
+    m_bQuit = true;
+  }
+  return true;
+}
+
+
+bool CbSlideRadial(void* pvGui,void* pvElem,int16_t nPos)
+{
+  gslc_tsGui*     pGui    = (gslc_tsGui*)(pvGui);
+  gslc_tsElem*    pElem   = (gslc_tsElem*)(pvElem);  
+  
+  char    acTxt[8];
+  int16_t nVal;
+  
+  // Fetch the new RGB component from the slider
+  switch (pElem->nId) {
+    case E_SLIDER:
+      nVal = gslc_ElemXSliderGetPos(pElem); 
+      
+      // Link slider to the radial control
+      gslc_tsElem* pElemRad = gslc_PageFindElemById(pGui,E_PG_MAIN,E_RADIAL);
+      gslc_ElemXGaugeUpdate(pElemRad,nVal);   
+      
+      // Link slider to the ramp control
+      gslc_tsElem* pElemRamp = gslc_PageFindElemById(pGui,E_PG_MAIN,E_RAMP);
+      gslc_ElemXGaugeUpdate(pElemRamp,nVal);   
+      
+      // Link slider to the numerical display
+      snprintf(acTxt,8,"%u",nVal);
+      gslc_tsElem* pElemCnt = gslc_PageFindElemById(pGui,E_PG_MAIN,E_ELEM_TXT_COUNT);      
+      gslc_ElemSetTxtStr(pElemCnt,acTxt);
+      break;
+
+    default:
+      break;
+  }
+  return true;
+}
+
+// Create page elements
+bool InitOverlays()
+{
+  gslc_tsElem*  pElem = NULL;
+
+  gslc_PageAdd(&m_gui,E_PG_MAIN,m_asPageElem,MAX_ELEM_PG_MAIN,m_asPageElemRef,MAX_ELEM_PG_MAIN);
+  
+  // Background flat color
+  gslc_SetBkgndColor(&m_gui,GSLC_COL_GRAY_DK2);
+
+  // Create Title with offset shadow
+  pElem = gslc_ElemCreateTxt(&m_gui,GSLC_ID_AUTO,E_PG_MAIN,(gslc_tsRect){2,2,320,50},
+    "Directional",0,E_FONT_TITLE);
+  gslc_ElemSetTxtCol(pElem,(gslc_tsColor){32,32,60});
+  gslc_ElemSetTxtAlign(pElem,GSLC_ALIGN_MID_MID);
+  gslc_ElemSetFillEn(pElem,false);
+  pElem = gslc_ElemCreateTxt(&m_gui,GSLC_ID_AUTO,E_PG_MAIN,(gslc_tsRect){0,0,320,50},
+    "Directional",0,E_FONT_TITLE);
+  gslc_ElemSetTxtCol(pElem,(gslc_tsColor){128,128,240});
+  gslc_ElemSetTxtAlign(pElem,GSLC_ALIGN_MID_MID);
+  gslc_ElemSetFillEn(pElem,false);
+    
+  // Create background box
+  pElem = gslc_ElemCreateBox(&m_gui,E_ELEM_BOX,E_PG_MAIN,(gslc_tsRect){10,50,300,180});
+  gslc_ElemSetCol(pElem,GSLC_COL_WHITE,GSLC_COL_BLACK,GSLC_COL_BLACK);
+
+  pElem = gslc_ElemXGaugeCreate(&m_gui,E_RADIAL,E_PG_MAIN,&m_sXGauge,
+          (gslc_tsRect){210,140,80,80},0,100,0,GSLC_COL_YELLOW,false);
+  gslc_ElemSetCol(pElem,GSLC_COL_WHITE,GSLC_COL_BLACK,GSLC_COL_BLACK);   
+  gslc_ElemXGaugeSetStyle(pElem,GSLCX_GAUGE_STYLE_RADIAL);
+  gslc_ElemXGaugeSetIndicator(pElem,GSLC_COL_YELLOW,30,3,true);
+  gslc_ElemXGaugeSetTicks(pElem,GSLC_COL_GRAY_LT1,8,5);
+  
+  pElem = gslc_ElemXGaugeCreate(&m_gui,E_RAMP,E_PG_MAIN,&m_sXGauge1,
+          (gslc_tsRect){80,140,100,80},0,100,50,GSLC_COL_YELLOW,false);
+  gslc_ElemSetCol(pElem,GSLC_COL_WHITE,GSLC_COL_BLACK,GSLC_COL_BLACK);   
+  gslc_ElemXGaugeSetStyle(pElem,GSLCX_GAUGE_STYLE_RAMP);
+ 
+  pElem = gslc_ElemXSliderCreate(&m_gui,E_SLIDER,E_PG_MAIN,&m_sXSlider,
+          (gslc_tsRect){20,60,140,20},0,100,50,5,false);
+  gslc_ElemSetCol(pElem,GSLC_COL_GREEN,GSLC_COL_BLACK,GSLC_COL_BLACK);
+  gslc_ElemXSliderSetStyle(pElem,true,GSLC_COL_GREEN_DK4,10,5,GSLC_COL_GRAY_DK2);
+  gslc_ElemXSliderSetPosFunc(pElem,&CbSlideRadial);    
+
+  pElem = gslc_ElemCreateTxt(&m_gui,E_ELEM_TXT_COUNT,E_PG_MAIN,(gslc_tsRect){180,60,40,20},
+    "",0,E_FONT_TXT); 
+  
+  pElem = gslc_ElemCreateBtnTxt(&m_gui,E_ELEM_BTN_QUIT,E_PG_MAIN,
+    (gslc_tsRect){250,60,50,30},"QUIT",0,E_FONT_BTN,&CbBtnQuit);
+  gslc_ElemSetCol(pElem,GSLC_COL_BLUE_DK2,GSLC_COL_BLUE_DK4,GSLC_COL_BLUE_DK1);    
+  gslc_ElemSetTxtCol(pElem,GSLC_COL_WHITE);  
+  
+  
+  return true;
+}
+
+
+int main( int argc, char* args[] )
+{
+  bool                bOk = true;
+  
+  // -----------------------------------
+  // Initialize
+  gslc_InitDebug(&DebugOut);
+  UserInitEnv();
+  if (!gslc_Init(&m_gui,&m_drv,m_asPage,MAX_PAGE,m_asFont,MAX_FONT)) { exit(1); }  
+  
+  // Load Fonts
+  // - Normally we would select a number of different fonts
+  bOk = gslc_FontAdd(&m_gui,E_FONT_BTN,FONT_DROID_SANS,14);
+  if (!bOk) { fprintf(stderr,"ERROR: FontAdd failed\n"); exit(1); }
+  bOk = gslc_FontAdd(&m_gui,E_FONT_TXT,FONT_DROID_SANS,10);
+  if (!bOk) { fprintf(stderr,"ERROR: FontAdd failed\n"); exit(1); }
+  bOk = gslc_FontAdd(&m_gui,E_FONT_HEAD,FONT_DROID_SANS,14);
+  if (!bOk) { fprintf(stderr,"ERROR: FontAdd failed\n"); exit(1); }
+  bOk = gslc_FontAdd(&m_gui,E_FONT_TITLE,FONT_DROID_SANS,36);
+  if (!bOk) { fprintf(stderr,"ERROR: FontAdd failed\n"); exit(1); }
+
+  // -----------------------------------
+  // Start display
+  InitOverlays();
+
+  // Start up display on main page
+  gslc_SetPageCur(&m_gui,E_PG_MAIN);
+    
+  // -----------------------------------
+  // Main event loop
+  while (!m_bQuit) {
+    gslc_Update(&m_gui);
+  }
+
+  // -----------------------------------
+  // Close down display
+  gslc_Quit(&m_gui);
+
+  return 0;
+}
---
+++ tests/gslc-ex10.c
@@ -0,0 +1,253 @@
+//
+// GUIslice Library Examples
+// - Calvin Hass
+// - http://www.impulseadventure.com/elec/guislice-gui.html
+// - Example 10 (LINUX):
+//     Demonstrate textbox controls
+//
+#include "GUIslice.h"
+#include "GUIslice_ex.h"
+#include "GUIslice_drv.h"
+
+#include "unistd.h"   // For usleep() //xxx
+
+// Defines for resources
+#define FONT_DROID_SANS "/usr/share/fonts/truetype/droid/DroidSans.ttf"
+
+// Enumerations for pages, elements, fonts, images
+enum {E_PG_MAIN};
+enum {E_ELEM_BOX,E_ELEM_BTN_QUIT,E_ELEM_COLOR,
+      E_SLIDER,E_ELEM_TXT_COUNT,
+      E_ELEM_TEXTBOX,E_SCROLLBAR};
+enum {E_FONT_BTN,E_FONT_TXT,E_FONT_HEAD,E_FONT_TITLE};
+
+bool      m_bQuit = false;
+
+// Free-running counter for display
+unsigned  m_nCount = 0;
+
+// Instantiate the GUI
+#define MAX_PAGE            1
+#define MAX_FONT            5
+#define MAX_ELEM_PG_MAIN    17
+
+gslc_tsGui                  m_gui;
+gslc_tsDriver               m_drv;
+gslc_tsFont                 m_asFont[MAX_FONT];
+gslc_tsPage                 m_asPage[MAX_PAGE];
+gslc_tsElem                 m_asPageElem[MAX_ELEM_PG_MAIN];
+gslc_tsElemRef              m_asPageElemRef[MAX_ELEM_PG_MAIN];
+
+gslc_tsXSlider              m_sXSlider;
+gslc_tsXSlider              m_sXSliderText;
+
+#define TBOX_ROWS       20
+#define TBOX_COLS       20
+gslc_tsXTextbox             m_sTextbox;
+char                        m_acTextboxBuf[TBOX_ROWS*TBOX_COLS];
+
+// Configure environment variables suitable for display
+// - These may need modification to match your system
+//   environment and display type
+// - Defaults for GSLC_DEV_FB and GSLC_DEV_TOUCH are in GUIslice_config.h
+// - Note that the environment variable settings can
+//   also be set directly within the shell via export
+//   (or init script).
+//   - eg. export TSLIB_FBDEVICE=/dev/fb1
+void UserInitEnv()
+{
+#if defined(DRV_DISP_SDL1) || defined(DRV_DISP_SDL2)
+  setenv((char*)"FRAMEBUFFER",GSLC_DEV_FB,1);
+  setenv((char*)"SDL_FBDEV",GSLC_DEV_FB,1);
+  setenv((char*)"SDL_VIDEODRIVER",GSLC_DEV_VID_DRV,1);
+#endif  
+
+#if defined(DRV_TOUCH_TSLIB)
+  setenv((char*)"TSLIB_FBDEVICE",GSLC_DEV_FB,1);
+  setenv((char*)"TSLIB_TSDEVICE",GSLC_DEV_TOUCH,1); 
+  setenv((char*)"TSLIB_CALIBFILE",(char*)"/etc/pointercal",1);
+  setenv((char*)"TSLIB_CONFFILE",(char*)"/etc/ts.conf",1);
+  setenv((char*)"TSLIB_PLUGINDIR",(char*)"/usr/local/lib/ts",1);  
+#endif
+   
+}
+
+// Define debug message function
+static int16_t DebugOut(char ch) { fputc(ch,stderr); return 0; }
+
+// Quit button callback
+bool CbBtnQuit(void* pvGui,void *pvElem,gslc_teTouch eTouch,int16_t nX,int16_t nY)
+{
+  if (eTouch == GSLC_TOUCH_UP_IN) {
+    m_bQuit = true;
+  }
+  return true;
+}
+
+
+bool CbControls(void* pvGui,void* pvElem,int16_t nPos)
+{
+  gslc_tsGui*     pGui    = (gslc_tsGui*)(pvGui);
+  gslc_tsElem*    pElem   = (gslc_tsElem*)(pvElem);  
+  
+  char            acTxt[20];
+  int16_t         nVal;
+  gslc_tsElem*    pElemTmp = NULL;
+  
+  // Handle various controls
+  switch (pElem->nId) {
+    case E_SCROLLBAR:
+      // Fetch the scrollbar value
+      nVal = gslc_ElemXSliderGetPos(pElem);
+      // Update the textbox scroll position
+      pElemTmp = gslc_PageFindElemById(pGui,E_PG_MAIN,E_ELEM_TEXTBOX);
+      gslc_ElemXTextboxScrollSet(pElemTmp,nVal,100);
+      break;
+      
+    case E_SLIDER:
+      // Fetch the slider position
+      nVal = gslc_ElemXSliderGetPos(pElem); 
+            
+      // Link slider to the numerical display
+      snprintf(acTxt,20,"%u",nVal);
+      pElemTmp = gslc_PageFindElemById(pGui,E_PG_MAIN,E_ELEM_TXT_COUNT);      
+      gslc_ElemSetTxtStr(pElemTmp,acTxt);
+      
+      // Link slider to insertion of text into textbox
+      pElemTmp = gslc_PageFindElemById(pGui,E_PG_MAIN,E_ELEM_TEXTBOX); 
+      snprintf(acTxt,20,"Slider=%3u\n",nVal);
+      gslc_ElemXTextboxAdd(pElemTmp,acTxt);
+
+      break;
+
+    default:
+      break;
+  }
+  return true;
+}
+
+// Create page elements
+bool InitOverlays()
+{
+  gslc_tsElem*  pElem = NULL;
+
+  gslc_PageAdd(&m_gui,E_PG_MAIN,m_asPageElem,MAX_ELEM_PG_MAIN,m_asPageElemRef,MAX_ELEM_PG_MAIN);
+  
+  // Background flat color
+  gslc_SetBkgndColor(&m_gui,GSLC_COL_GRAY_DK2);
+
+  // Create Title with offset shadow
+  pElem = gslc_ElemCreateTxt(&m_gui,GSLC_ID_AUTO,E_PG_MAIN,(gslc_tsRect){2,2,320,50},
+    "Textbox",0,E_FONT_TITLE);
+  gslc_ElemSetTxtCol(pElem,(gslc_tsColor){32,32,60});
+  gslc_ElemSetTxtAlign(pElem,GSLC_ALIGN_MID_MID);
+  gslc_ElemSetFillEn(pElem,false);
+  pElem = gslc_ElemCreateTxt(&m_gui,GSLC_ID_AUTO,E_PG_MAIN,(gslc_tsRect){0,0,320,50},
+    "Textbox",0,E_FONT_TITLE);
+  gslc_ElemSetTxtCol(pElem,(gslc_tsColor){128,128,240});
+  gslc_ElemSetTxtAlign(pElem,GSLC_ALIGN_MID_MID);
+  gslc_ElemSetFillEn(pElem,false);
+    
+  // Create background box
+  pElem = gslc_ElemCreateBox(&m_gui,E_ELEM_BOX,E_PG_MAIN,(gslc_tsRect){10,50,300,180});
+  gslc_ElemSetCol(pElem,GSLC_COL_WHITE,GSLC_COL_BLACK,GSLC_COL_BLACK);
+
+  // Example horizontal slider
+  pElem = gslc_ElemXSliderCreate(&m_gui,E_SLIDER,E_PG_MAIN,&m_sXSlider,
+          (gslc_tsRect){20,60,140,20},0,100,50,5,false);
+  gslc_ElemSetCol(pElem,GSLC_COL_GREEN,GSLC_COL_BLACK,GSLC_COL_BLACK);
+  gslc_ElemXSliderSetStyle(pElem,true,GSLC_COL_GREEN_DK4,10,5,GSLC_COL_GRAY_DK2);
+  gslc_ElemXSliderSetPosFunc(pElem,&CbControls);    
+
+  // Text to show slider value
+  pElem = gslc_ElemCreateTxt(&m_gui,E_ELEM_TXT_COUNT,E_PG_MAIN,(gslc_tsRect){180,60,40,20},
+    "",0,E_FONT_TXT); 
+  
+  
+  // Create wrapping box for textbox and scrollbar
+  pElem = gslc_ElemCreateBox(&m_gui,GSLC_ID_AUTO,E_PG_MAIN,(gslc_tsRect){18,83,203,124});
+  gslc_ElemSetCol(pElem,GSLC_COL_BLUE_DK4,GSLC_COL_BLACK,GSLC_COL_BLACK);
+  
+  // Create textbox
+  pElem = gslc_ElemXTextboxCreate(&m_gui,E_ELEM_TEXTBOX,E_PG_MAIN,
+    &m_sTextbox,(gslc_tsRect){20,85,180,120},E_FONT_TXT,(char*)&m_acTextboxBuf,
+        TBOX_ROWS,TBOX_COLS);
+  gslc_ElemXTextboxWrapSet(pElem,true);
+  gslc_ElemSetCol(pElem,GSLC_COL_BLUE_LT2,GSLC_COL_BLACK,GSLC_COL_GRAY_DK3);  
+ 
+  // Create vertical scrollbar for textbox
+  pElem = gslc_ElemXSliderCreate(&m_gui,E_SCROLLBAR,E_PG_MAIN,&m_sXSliderText,
+        (gslc_tsRect){200,85,20,120},0,100,100,5,true);
+  gslc_ElemSetCol(pElem,GSLC_COL_BLUE_DK4,GSLC_COL_BLACK,GSLC_COL_BLACK);
+  gslc_ElemXSliderSetPosFunc(pElem,&CbControls); 
+  
+  // Quit button
+  pElem = gslc_ElemCreateBtnTxt(&m_gui,E_ELEM_BTN_QUIT,E_PG_MAIN,
+    (gslc_tsRect){250,60,50,30},"QUIT",0,E_FONT_BTN,&CbBtnQuit);
+  gslc_ElemSetCol(pElem,GSLC_COL_BLUE_DK2,GSLC_COL_BLUE_DK4,GSLC_COL_BLUE_DK1);    
+  gslc_ElemSetTxtCol(pElem,GSLC_COL_WHITE);  
+
+  return true;
+}
+
+
+int main( int argc, char* args[] )
+{
+  bool                bOk = true;
+  
+  // -----------------------------------
+  // Initialize
+  gslc_InitDebug(&DebugOut);
+  UserInitEnv();
+  if (!gslc_Init(&m_gui,&m_drv,m_asPage,MAX_PAGE,m_asFont,MAX_FONT)) { exit(1); }  
+  
+  // Load Fonts
+  // - Normally we would select a number of different fonts
+  bOk = gslc_FontAdd(&m_gui,E_FONT_BTN,FONT_DROID_SANS,14);
+  if (!bOk) { fprintf(stderr,"ERROR: FontAdd failed\n"); exit(1); }
+  bOk = gslc_FontAdd(&m_gui,E_FONT_TXT,FONT_DROID_SANS,10);
+  if (!bOk) { fprintf(stderr,"ERROR: FontAdd failed\n"); exit(1); }
+  bOk = gslc_FontAdd(&m_gui,E_FONT_HEAD,FONT_DROID_SANS,14);
+  if (!bOk) { fprintf(stderr,"ERROR: FontAdd failed\n"); exit(1); }
+  bOk = gslc_FontAdd(&m_gui,E_FONT_TITLE,FONT_DROID_SANS,36);
+  if (!bOk) { fprintf(stderr,"ERROR: FontAdd failed\n"); exit(1); }
+
+  // -----------------------------------
+  // Start display
+  InitOverlays();
+
+  // Start up display on main page
+  gslc_SetPageCur(&m_gui,E_PG_MAIN);
+  
+  // Insert some text
+  gslc_tsElem* pElemTextbox = gslc_PageFindElemById(&m_gui,E_PG_MAIN,E_ELEM_TEXTBOX); 
+
+  gslc_ElemXTextboxAdd(pElemTextbox,"Hi there!\n");
+  gslc_ElemXTextboxAdd(pElemTextbox,"Status: ");
+  gslc_ElemXTextboxColSet(pElemTextbox,GSLC_COL_RED);
+  gslc_ElemXTextboxAdd(pElemTextbox,"FAIL\n");  
+  gslc_ElemXTextboxColReset(pElemTextbox);
+  gslc_ElemXTextboxAdd(pElemTextbox,"Long line here that might wrap\n");
+  gslc_ElemXTextboxAdd(pElemTextbox,"Goodbye...\n");  
+  
+  // -----------------------------------
+  // Main event loop
+  uint16_t  nCnt = 0;
+  char  acTxt[20];
+  while (!m_bQuit) {
+    gslc_Update(&m_gui);
+    
+    usleep(50);
+    nCnt++;
+    if ((nCnt % 5000) == 0) {
+      snprintf(acTxt,20,"%u\n",nCnt);
+      gslc_ElemXTextboxAdd(pElemTextbox,acTxt);
+    }
+  }
+
+  // -----------------------------------
+  // Close down display
+  gslc_Quit(&m_gui);
+
+  return 0;
+}
