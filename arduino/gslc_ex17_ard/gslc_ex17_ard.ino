17f43c80ef4da5eb51f7e27861686e843b68f181
ImpulseAdventure <web@impulseadventure.com>
2018-10-30 23:06:32 -0700

    Renamed gslc_ex17_ard to gslc_ex19_ard
    - Original gslc_ex17_ard created by lukasfischer83 for PR #63
    - Renamed file to avoid collision from concurrent PRs

--- arduino/gslc_ex17_ard/gslc_ex17_ard.ino
+++ arduino/gslc_ex17_ard/gslc_ex17_ard.ino
@@ -3,41 +3,35 @@
 // - Calvin Hass
 // - https://www.impulseadventure.com/elec/guislice-gui.html
 // - https://github.com/ImpulseAdventure/GUIslice
-// - Example 17 (ESP32):
-//   - Accept touch input, text button, text box
-//   - Antialiased Fonts
-//   - NOTE: This is Example demonstrates text antialiasing with the TFT_eSPI library.
-//     The font which can be found in the TFT_eSPI source in the libraries
-//     examples/Smooth Fonts/Print_Smooth_Font/data folder has to be uploaded to SPIFFS first
+// - Example 17 (Arduino):
+//   - Show the usage of dynamic rotation
+//   - Accept touch input, text button
+//   - Expected behavior: Clicking on button rotates the screen
+//
 
-// Font file is stored in SPIFFS
+// ARDUINO NOTES:
+// - GUIslice_config.h must be edited to match the pinout connections
+//   between the Arduino CPU and the display controller (see ADAGFX_PIN_*).
 
-#if !defined(ESP8266) && !defined(ESP32)
-  #error "This example only works on ESP8266 / ESP32"
-#endif
-
-
-#define FS_NO_GLOBALS
-#include <SPIFFS.h>
-
-#include <stdint.h>
 #include "GUIslice.h"
 #include "GUIslice_ex.h"
 #include "GUIslice_drv.h"
+
 
 // Defines for resources
 
 // Enumerations for pages, elements, fonts, images
 enum {E_PG_MAIN};
-enum {E_ELEM_BOX, E_ELEM_BTN_QUIT1, E_ELEM_BTN_QUIT2, E_ELEM_TEXT};
-enum {E_FONT_BTN, E_FONT_AATEXT};
+enum {E_ELEM_BOX,E_ELEM_BTN_ROTATE};
+enum {E_FONT_BTN};
 
-bool    m_bQuit = false;
+bool      m_bRotate = false;
+uint8_t   m_nRotation = 1;
 
 // Instantiate the GUI
 #define MAX_PAGE            1
-#define MAX_FONT            2
-#define MAX_ELEM_PG_MAIN    4
+#define MAX_FONT            1
+#define MAX_ELEM_PG_MAIN    2
 
 gslc_tsGui                  m_gui;
 gslc_tsDriver               m_drv;
@@ -46,44 +40,35 @@ gslc_tsPage                 m_asPage[MAX_PAGE];
 gslc_tsElem                 m_asPageElem[MAX_ELEM_PG_MAIN];
 gslc_tsElemRef              m_asPageElemRef[MAX_ELEM_PG_MAIN];
 
+
+
 // Define debug message function
 static int16_t DebugOut(char ch) { Serial.write(ch); return 0; }
 
 // Button callbacks
-bool CbBtnQuit(void* pvGui,void *pvElem,gslc_teTouch eTouch,int16_t nX,int16_t nY)
+bool CbBtnRotate(void* pvGui,void *pvElem,gslc_teTouch eTouch,int16_t nX,int16_t nY)
 {
   if (eTouch == GSLC_TOUCH_UP_IN) {
-    m_bQuit = true;
+    m_bRotate = true;
   }
   return true;
 }
 
 void setup()
 {
-
-  bool            bOk = true;
   gslc_tsElemRef* pElemRef = NULL;
 
   // Initialize debug output
-  analogReadResolution(10);
   Serial.begin(9600);
   gslc_InitDebug(&DebugOut);
   //delay(1000);  // NOTE: Some devices require a delay after Serial.begin() before serial port can be used
-
-
-  // Init SPIFFS for Font Loading
-  if (!SPIFFS.begin()) {
-    Serial.println("SPIFFS initialisation failed!");
-    while (1) yield(); // Stay here twiddling thumbs waiting
-  }
 
 
   // Initialize
   if (!gslc_Init(&m_gui,&m_drv,m_asPage,MAX_PAGE,m_asFont,MAX_FONT)) { return; }
 
   // Load Fonts
-  if (!gslc_FontAdd(&m_gui,E_FONT_BTN,GSLC_FONTREF_PTR,NULL,3)) { return; }
-  if (!gslc_FontAdd(&m_gui,E_FONT_AATEXT,GSLC_FONTREF_FNAME,"Final-Frontier-28",28)) { return; }
+  if (!gslc_FontAdd(&m_gui,E_FONT_BTN,GSLC_FONTREF_PTR,NULL,1)) { return; }
 
   // -----------------------------------
   // Create page elements
@@ -93,27 +78,20 @@ void setup()
   gslc_SetBkgndColor(&m_gui,GSLC_COL_GRAY_DK2);
 
   // Create background box
-  pElemRef = gslc_ElemCreateBox(&m_gui,E_ELEM_BOX,E_PG_MAIN,(gslc_tsRect){10,20,300,200});
+  pElemRef = gslc_ElemCreateBox(&m_gui,E_ELEM_BOX,E_PG_MAIN,(gslc_tsRect){10,50,220,150});
   gslc_ElemSetCol(&m_gui,pElemRef,GSLC_COL_WHITE,GSLC_COL_BLACK,GSLC_COL_BLACK);
 
-  // Create Quit button with text label
-  pElemRef = gslc_ElemCreateBtnTxt(&m_gui,E_ELEM_BTN_QUIT1,E_PG_MAIN,
-    (gslc_tsRect){120,40,80,40},(char*)"Quit",0,E_FONT_BTN,&CbBtnQuit);
-
-  pElemRef = gslc_ElemCreateBtnTxt(&m_gui,E_ELEM_BTN_QUIT2,E_PG_MAIN,
-    (gslc_tsRect){120,100,80,40},(char*)"Quit",0,E_FONT_AATEXT,&CbBtnQuit);
-
-  pElemRef = gslc_ElemCreateTxt(&m_gui,E_ELEM_TEXT,E_PG_MAIN,
-    (gslc_tsRect){30,160,260,40},(char*)"Antialias Demo Text",0,E_FONT_AATEXT);
-
-
+  // Create rotate button with text label
+  pElemRef = gslc_ElemCreateBtnTxt(&m_gui,E_ELEM_BTN_ROTATE,E_PG_MAIN,
+    (gslc_tsRect){20,60,80,40},(char*)"Rotate",0,E_FONT_BTN,&CbBtnRotate);  //place the botton out of the middle to be able to check if flipping / swapping is correct
 
   // -----------------------------------
   // Start up display on main page
   gslc_SetPageCur(&m_gui,E_PG_MAIN);
 
-  m_bQuit = false;
+  m_bRotate = false;
 }
+
 
 void loop()
 {
@@ -121,11 +99,18 @@ void loop()
   gslc_Update(&m_gui);
 
   // In a real program, we would detect the button press and take an action.
-  // For this Arduino demo, we will pretend to exit by emulating it with an
-  // infinite loop. Note that interrupts are not disabled so that any debug
-  // messages via Serial have an opportunity to be transmitted.
-  if (m_bQuit) {
-    gslc_Quit(&m_gui);
-    while (1) { }
+  // For this Arduino demo, we change the rotation
+
+  if (m_bRotate) {
+    gslc_GuiRotate(&m_gui, m_nRotation);
+    m_nRotation = (m_nRotation+1) % 4;
+
+    m_bRotate = false;
+
   }
+  //delay(1000);
+
 }
+
+
+
---
+++ arduino/gslc_ex19_ard/gslc_ex19_ard.ino
@@ -0,0 +1,131 @@
+//
+// GUIslice Library Examples
+// - Calvin Hass
+// - https://www.impulseadventure.com/elec/guislice-gui.html
+// - https://github.com/ImpulseAdventure/GUIslice
+// - Example 19 (ESP8266/ESP32):
+//   - Accept touch input, text button, text box
+//   - Antialiased Fonts
+//   - NOTE: This is Example demonstrates text antialiasing with the TFT_eSPI library.
+//     The font which can be found in the TFT_eSPI source in the libraries
+//     examples/Smooth Fonts/Print_Smooth_Font/data folder has to be uploaded to SPIFFS first
+
+// Font file is stored in SPIFFS
+
+#if !defined(ESP8266) && !defined(ESP32)
+  #error "This example only works on ESP8266 / ESP32"
+#endif
+
+
+#define FS_NO_GLOBALS
+#include <SPIFFS.h>
+
+#include <stdint.h>
+#include "GUIslice.h"
+#include "GUIslice_ex.h"
+#include "GUIslice_drv.h"
+
+// Defines for resources
+
+// Enumerations for pages, elements, fonts, images
+enum {E_PG_MAIN};
+enum {E_ELEM_BOX, E_ELEM_BTN_QUIT1, E_ELEM_BTN_QUIT2, E_ELEM_TEXT};
+enum {E_FONT_BTN, E_FONT_AATEXT};
+
+bool    m_bQuit = false;
+
+// Instantiate the GUI
+#define MAX_PAGE            1
+#define MAX_FONT            2
+#define MAX_ELEM_PG_MAIN    4
+
+gslc_tsGui                  m_gui;
+gslc_tsDriver               m_drv;
+gslc_tsFont                 m_asFont[MAX_FONT];
+gslc_tsPage                 m_asPage[MAX_PAGE];
+gslc_tsElem                 m_asPageElem[MAX_ELEM_PG_MAIN];
+gslc_tsElemRef              m_asPageElemRef[MAX_ELEM_PG_MAIN];
+
+// Define debug message function
+static int16_t DebugOut(char ch) { Serial.write(ch); return 0; }
+
+// Button callbacks
+bool CbBtnQuit(void* pvGui,void *pvElem,gslc_teTouch eTouch,int16_t nX,int16_t nY)
+{
+  if (eTouch == GSLC_TOUCH_UP_IN) {
+    m_bQuit = true;
+  }
+  return true;
+}
+
+void setup()
+{
+
+  bool            bOk = true;
+  gslc_tsElemRef* pElemRef = NULL;
+
+  // Initialize debug output
+  analogReadResolution(10);
+  Serial.begin(9600);
+  gslc_InitDebug(&DebugOut);
+  //delay(1000);  // NOTE: Some devices require a delay after Serial.begin() before serial port can be used
+
+
+  // Init SPIFFS for Font Loading
+  if (!SPIFFS.begin()) {
+    Serial.println("SPIFFS initialisation failed!");
+    while (1) yield(); // Stay here twiddling thumbs waiting
+  }
+
+
+  // Initialize
+  if (!gslc_Init(&m_gui,&m_drv,m_asPage,MAX_PAGE,m_asFont,MAX_FONT)) { return; }
+
+  // Load Fonts
+  if (!gslc_FontAdd(&m_gui,E_FONT_BTN,GSLC_FONTREF_PTR,NULL,3)) { return; }
+  if (!gslc_FontAdd(&m_gui,E_FONT_AATEXT,GSLC_FONTREF_FNAME,"Final-Frontier-28",28)) { return; }
+
+  // -----------------------------------
+  // Create page elements
+  gslc_PageAdd(&m_gui,E_PG_MAIN,m_asPageElem,MAX_ELEM_PG_MAIN,m_asPageElemRef,MAX_ELEM_PG_MAIN);
+
+  // Background flat color
+  gslc_SetBkgndColor(&m_gui,GSLC_COL_GRAY_DK2);
+
+  // Create background box
+  pElemRef = gslc_ElemCreateBox(&m_gui,E_ELEM_BOX,E_PG_MAIN,(gslc_tsRect){10,20,300,200});
+  gslc_ElemSetCol(&m_gui,pElemRef,GSLC_COL_WHITE,GSLC_COL_BLACK,GSLC_COL_BLACK);
+
+  // Create Quit button with text label
+  pElemRef = gslc_ElemCreateBtnTxt(&m_gui,E_ELEM_BTN_QUIT1,E_PG_MAIN,
+    (gslc_tsRect){120,40,80,40},(char*)"Quit",0,E_FONT_BTN,&CbBtnQuit);
+
+  pElemRef = gslc_ElemCreateBtnTxt(&m_gui,E_ELEM_BTN_QUIT2,E_PG_MAIN,
+    (gslc_tsRect){120,100,80,40},(char*)"Quit",0,E_FONT_AATEXT,&CbBtnQuit);
+
+  pElemRef = gslc_ElemCreateTxt(&m_gui,E_ELEM_TEXT,E_PG_MAIN,
+    (gslc_tsRect){30,160,260,40},(char*)"Antialias Demo Text",0,E_FONT_AATEXT);
+
+
+
+  // -----------------------------------
+  // Start up display on main page
+  gslc_SetPageCur(&m_gui,E_PG_MAIN);
+
+  m_bQuit = false;
+}
+
+void loop()
+{
+  // Periodically call GUIslice update function
+  gslc_Update(&m_gui);
+
+  // In a real program, we would detect the button press and take an action.
+  // For this Arduino demo, we will pretend to exit by emulating it with an
+  // infinite loop. Note that interrupts are not disabled so that any debug
+  // messages via Serial have an opportunity to be transmitted.
+  if (m_bQuit) {
+    gslc_Quit(&m_gui);
+    while (1) { }
+  }
+}
