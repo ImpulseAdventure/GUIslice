30dad070024be81379a4e5fcfa1a258c3c5b9508
Calvin Hass <web@impulseadventure.com>
2019-01-12 08:38:42 -0800

    Integrate example config for Raspberry Pi

--- src/GUIslice_config.h
+++ src/GUIslice_config.h
@@ -95,6 +95,7 @@ extern "C" {
 
   // Raspberry Pi / LINUX:
   // ------------------------------------------------------
+  //#include "../configs/rpi-sdl1-default-tslib.h"
   //#include "../configs/linux-sdl1-default-mouse.h"
 
 
