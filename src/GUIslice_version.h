48bd7cefeb6c1150cef6e7eaaac982dfc18ceb40
Calvin Hass <web@impulseadventure.com>
2019-02-18 19:15:21 -0800

    Add ard-adagfx-ra8875-notouch to config selection

--- src/GUIslice_version.h
+++ src/GUIslice_version.h
@@ -36,7 +36,7 @@
 // Define current release (X.Y.Z) & build number
 // =======================================================================
 
-#define GUISLICE_VER "0.11.0.45"
+#define GUISLICE_VER "0.11.0.46"
 
 #endif // _GUISLICE_VERSION_H_
 
