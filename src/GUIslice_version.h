532c344c3d9a9094abc5f902bbfbf4889fed0ba9
Calvin Hass <web@impulseadventure.com>
2019-07-06 00:40:48 -0700

    M5stack: Fix SD low-level read routines - DrvRead16/32SD() param reference

--- src/GUIslice_version.h
+++ src/GUIslice_version.h
@@ -36,7 +36,7 @@
 // Define current release (X.Y.Z) & build number
 // =======================================================================
 
-#define GUISLICE_VER "0.12.2.5"
+#define GUISLICE_VER "0.12.2.6"
 
 #endif // _GUISLICE_VERSION_H_
 
