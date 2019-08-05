aeb4cd52a2b7f0c90bfcd589e10b2228a0b46e20
Calvin Hass <web@impulseadventure.com>
2019-08-05 14:55:59 -0700

    Update LINUX examples for revised APIs

--- src/GUIslice_version.h
+++ src/GUIslice_version.h
@@ -36,7 +36,7 @@
 // Define current release (X.Y.Z) & build number
 // =======================================================================
 
-#define GUISLICE_VER "0.12.2.45"
+#define GUISLICE_VER "0.12.2.46"
 
 #endif // _GUISLICE_VERSION_H_
 
