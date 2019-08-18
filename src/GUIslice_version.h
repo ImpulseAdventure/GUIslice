52012849fd66f29237bbcf4f81ea96415346f975
Calvin Hass <web@impulseadventure.com>
2019-08-18 09:52:05 -0700

    SDL: Add indicator for DRV_HAS_DRAW_RECT_ROUND_*

--- src/GUIslice_version.h
+++ src/GUIslice_version.h
@@ -36,7 +36,7 @@
 // Define current release (X.Y.Z) & build number
 // =======================================================================
 
-#define GUISLICE_VER "0.12.2.70"
+#define GUISLICE_VER "0.12.2.71"
 
 #endif // _GUISLICE_VERSION_H_
 
