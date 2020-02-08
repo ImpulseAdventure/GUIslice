e24b1d0153f82bb5fcf7d69125a4cca73371780d
Calvin Hass <web@impulseadventure.com>
2020-02-07 19:30:39 -0800

    RA8876/FT5206 config update

--- configs/ard-adagfx-ra8876-ft5206.h
+++ configs/ard-adagfx-ra8876-ft5206.h
@@ -80,10 +80,10 @@ extern "C" {
   // For shields, the following pinouts are typically hardcoded
   // These values were defined to match an Arduino Zero config
   #define ADAGFX_PIN_CS       11    // Display chip select
-  #define ADAGFX_PIN_RST      12    // Display Reset
+  #define ADAGFX_PIN_RST      0     // Display Reset
 
   // SD Card
-  #define ADAGFX_PIN_SDCS     4     // SD card chip select (if GSLC_SD_EN=1)
+  #define ADAGFX_PIN_SDCS     12    // SD card chip select (if GSLC_SD_EN=1)
 
 
 
