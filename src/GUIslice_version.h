6c3b8441928d738540cb2419d1adce1d56e14645
Calvin Hass <ImpulseAdventure@users.noreply.github.com>
2020-03-03 06:52:03 -0700

    Update version

--- src/GUIslice_version.h
+++ src/GUIslice_version.h
@@ -36,7 +36,7 @@
 // Define current release (X.Y.Z) & build number
 // =======================================================================
 
-#define GUISLICE_VER "0.13.0.19"
+#define GUISLICE_VER "0.13.0.20"
 
 #endif // _GUISLICE_VERSION_H_
 
