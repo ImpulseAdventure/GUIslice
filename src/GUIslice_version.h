8f250ea9c964d8cd482e3a5a2d3155d3b7876db9
Calvin Hass <web@impulseadventure.com>
2020-11-05 21:56:14 -0800

    PlatformIO config cleanup
    - Inspect / cppcheck works with -flto build_unflags
    - Reset default_envs

--- src/GUIslice_version.h
+++ src/GUIslice_version.h
@@ -36,7 +36,7 @@
 // Define current release (X.Y.Z) & build number
 // =======================================================================
 
-#define GUISLICE_VER "0.15.1.11"
+#define GUISLICE_VER "0.15.1.12"
 
 #endif // _GUISLICE_VERSION_H_
 
