b2be1c15a39206be96077f7c0e56443f0928814b
Calvin Hass <web@impulseadventure.com>
2021-03-16 10:16:53 -0700

    Update version

--- src/GUIslice_version.h
+++ src/GUIslice_version.h
@@ -36,7 +36,7 @@
 // Define current release (X.Y.Z) & build number
 // =======================================================================
 
-#define GUISLICE_VER "0.16.0.18"
+#define GUISLICE_VER "0.16.0.19"
 
 #endif // _GUISLICE_VERSION_H_
 
